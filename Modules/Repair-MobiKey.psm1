4b9b0419185383083777ef5cc1745a937ca46ef1
Erik Arnesen <earnesen@ymail.com>
2019-09-01 20:27:50 -0400

    Cleaned up and renamed
    Cleaned up the code and renamed.  Added link to wiki

--- Modules/Repair-MobiKey.psm1
+++ Modules/Repair-MobiKey.psm1
@@ -8,22 +8,29 @@ function Repair-MobiKey
    
       .EXAMPLE
       Repair-MobiKey
+
+      Repair-MobiKey -MobikeyCertsPath '\\Networkshare\mobikey\certs'
          
       .NOTES
-      Place additional notes here.
+      You will need to make the following changes
 
       .LINK
-      https://github.com/KnarrStudio/PS-Scripts
+      https://github.com/KnarrStudio/PS-Scripts/wiki/Repair-MobiKey
+
       The first link is opened by Get-Help -Online Repair-MobiKey
 
   #>
 
-  [CmdletBinding(HelpUri = 'https://github.com/KnarrStudio/PS-Scripts',
-  ConfirmImpact='Medium')]
+  [CmdletBinding(HelpUri = 'https://github.com/KnarrStudio/PS-Scripts/wiki/MobiKey-Repair',
+  ConfirmImpact = 'Medium')]
   [OutputType([String])]
-   
-   
-  $MobikeyNetworkPath = '\\Networkshare\mobikey\certs'
+  Param
+  (
+    # Param1 help description
+    [Parameter(Mandatory = $false, Position = 0)]
+    [string]$MobikeyCertsPath = '\\Networkshare\mobikey\certs'
+  )
+  
   $ServiceName = 'Route1*'
   $MobikeyLocalPath = "$env:ProgramData\Mobikey\certs"
   
@@ -33,35 +40,45 @@ function Repair-MobiKey
   Stop-Service -Name $ServiceName
      
   # Delete the old files out of the ProgramData Dir
-  if (Test-Path -Path $MobikeyLocalPath){
+  if (Test-Path -Path $MobikeyLocalPath)
+  {
     Write-Verbose -Message ('Delete old Certificate files')
     Get-ChildItem -Path $MobikeyLocalPath -Recurse | Remove-Item -Force -WhatIf
   }
   
   # Copy files from the File share to the local drive
-  if (Test-Path -Path $MobikeyNetworkPath){
+  if (Test-Path -Path $MobikeyCertsPath)
+  {
     Write-Verbose -Message ('Copy Files from Network to Localhost')
-    Copy-Item -Path $MobikeyNetworkPath -Include *.* -Destination $MobikeyLocalPath -Force
-    $MobiKeyCerts = gci -Path .\ 
+    Copy-Item -Path $MobikeyCertsPath -Include *.* -Destination $MobikeyLocalPath -Force
+    # $MobiKeyCerts = Get-ChildItem -Path .\ 
   }
   # Start the Mobikey service 
   Write-Verbose -Message ('Start Mobikey Service')
   Start-Service -Name $ServiceName
 
 
-  # Commit Point
   # Working on testing dates to compare files
-  foreach($MobiKeyCert in $MobiKeyCerts){
-  $MobiKeyCertOrgDate = $MobiKeyCertDate.lastwritetime
+  <#  foreach($MobiKeyCert in $MobiKeyCerts)
+      {
+      $MobiKeyCertOrgDate = $MobiKeyCertDate.lastwritetime
 
-  For($i=0;$i -lt 6;$i++){
-    if(($f[$i].CreationTimeUtc) -ne ($f[$i].lastwritetime)){
-        Write-Host ('not equal {0} -- {1}' -f $f[$i].CreationTimeUtc, $f[$i].lastwritetime)}}
-
-
-  }
+      For($i = 0;$i -lt 6;$i++)
+      {
+      if(($f[$i].CreationTimeUtc) -ne ($f[$i].lastwritetime))
+      {
+      Write-Host ('not equal {0} -- {1}' -f $f[$i].CreationTimeUtc, $f[$i].lastwritetime)
+      }
+      }
+  }#>
    
   $ServiceStatus = (Get-Service -Name $ServiceName).Status
   Write-Verbose -Message ('Mobikey Service is {0}' -f $ServiceStatus)
-
 }
+
+
+
+
+
+
+
--- Repair-MobiKey.ps1
+++ Repair-MobiKey.ps1
@@ -1,39 +1,79 @@
 #requires -runasadministrator
 
+function Repair-MobiKey 
+{
   <#
       .SYNOPSIS
       Repairs the Mobkey Installation
    
-      .DESCRIPTION
-      Add a more complete description of what the function does.
-
       .EXAMPLE
       Repair-MobiKey
+
+      Repair-MobiKey -MobikeyCertsPath '\\Networkshare\mobikey\certs'
          
       .NOTES
-      Place additional notes here.
+      You will need to make the following changes
 
       .LINK
-      URLs to related sites
+      https://github.com/KnarrStudio/PS-Scripts/wiki/Repair-MobiKey
+
       The first link is opened by Get-Help -Online Repair-MobiKey
 
   #>
-   
-  # First Stop the Mobikey Service 
-  $MobikeyCertPath = "$env:ProgramData\Mobikey\certs"
-  Stop-Service -Name 'Route1*'
-   
-  # Delete the old files out of the ProgramData Dir
-  if (Test-Path -Path $MobikeyCertPath){
-    Get-ChildItem -Path $MobikeyCertPath -Recurse | Remove-Item -Force -WhatIf
+
+  [CmdletBinding(HelpUri = 'https://github.com/KnarrStudio/PS-Scripts/wiki/MobiKey-Repair',
+  ConfirmImpact = 'Medium')]
+  [OutputType([String])]
+  Param
+  (
+    # Param1 help description
+    [Parameter(Mandatory, Position = 0)]
+    [string]$MobikeyCertsPath 
+  )
+  
+  Begin
+  {
+
+    $ServiceName = 'Route1*'
+    $MobikeyLocalPath = "$env:ProgramData\Mobikey\certs"
+  
   }
-  # Copy files from the File share to the local drive
+  Process
+  {
 
-  Copy-Item -Path \\resorce\share\mobikeey\certs -Include *.* -Destination $env:ProgramData\mobikey\certs -Force
+    # First Stop the Mobikey Service 
+    Write-Verbose -Message ('Stop Mobikey Service')
+    Stop-Service -Name $ServiceName
+     
+    # Delete the old files out of the ProgramData Dir
+    if (Test-Path -Path $MobikeyLocalPath)
+    {
+      Write-Verbose -Message ('Delete old Certificate files')
+      Get-ChildItem -Path $MobikeyLocalPath -Recurse | Remove-Item -Force -WhatIf
+    }
+  
+    # Copy files from the File share to the local drive
+    if (Test-Path -Path $MobikeyCertsPath)
+    {
+      Write-Verbose -Message ('Copy Files from Network to Localhost')
+      Copy-Item -Path $MobikeyCertsPath -Include *.* -Destination $MobikeyLocalPath -Force
+    }
+    # Start the Mobikey service 
+    Write-Verbose -Message ('Start Mobikey Service')
+    Start-Service -Name $ServiceName
 
-  # Start the Mobikey service 
-  Start-Service -Name 'Route1*'
-   
-  $ServiceStatus = (Get-Service -Name 'Route1*').Status
-  Write-Host ('Mobikey Service is {0}' -f $ServiceStatus)
+  }
+  End
+  {
+    
+    $ServiceStatus = (Get-Service -Name $ServiceName).Status
+    Write-Verbose -Message ('Mobikey Service is {0}' -f $ServiceStatus)
+  }
+}
+
+
+Repair-MobiKey -MobikeyCertsPath '\\Networkshare\mobikey\certs'
+
+
+
 
