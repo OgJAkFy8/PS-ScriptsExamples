ee6b2c4bc9a46bf3cde45bd3b294edeb369ce7fb
Erik Arnesen <earnesen@ymail.com>
2019-09-15 16:36:06 -0400

    Update

--- Active Repair Projects/Uninstall-Hotfix.ps1
+++ Active Repair Projects/Uninstall-Hotfix.ps1
@@ -1,29 +1,50 @@
-function Uninstall-Hotfix {
-[cmdletbinding()]
-param(
-$computername = $env:computername,
-[string] $HotfixID
-)            
+## Not operational Might be a good start
 
-$hotfixes = Get-WmiObject -ComputerName $computername -Class Win32_QuickFixEngineering | select hotfixid            
 
-if($hotfixes -match $hotfixID) {
-    $hotfixID = $HotfixID.Replace("KB","")
-    Write-host "Found the hotfix KB" + $HotfixID
-    Write-Host "Uninstalling the hotfix"
-    $UninstallString = "cmd.exe /c wusa.exe /uninstall /KB:$hotfixID /quiet /norestart"
-    ([WMICLASS]"\\$computername\ROOT\CIMV2:win32_process").Create($UninstallString) | out-null            
 
-    while (@(Get-Process wusa -computername $computername -ErrorAction SilentlyContinue).Count -ne 0) {
-        Start-Sleep 3
-        Write-Host "Waiting for update removal to finish ..."
+#requires -Version 3.0
+function Uninstall-Hotfix 
+{
+  <#
+      .SYNOPSIS
+      Describe purpose of "Uninstall-Hotfix" in 1-2 sentences.
+  #>
+
+
+  [cmdletbinding()]
+  Param(
+    [Parameter(Position=0)]
+    [string] $computername = $env:computername,
+    [Parameter(Mandatory,HelpMessage='Enter the Hotfix with or without "KB"')]
+    [string] $HotfixID = 'KB4516058'
+  )            
+
+
+  $hotfixes = Get-WmiObject -ComputerName $computername -Class Win32_QuickFixEngineering | Select-Object -ExpandProperty hotfixid 
+
+  if($hotfixes -match $HotfixID) 
+  {
+    $HotfixID = $HotfixID.Replace('KB','')
+    Write-Output -InputObject ('Found the hotfix {0}' -f $HotfixID) 
+    
+    Write-Verbose -Message 'Uninstalling the hotfix'
+    $UninstallString = "cmd.exe /c wusa.exe /uninstall /KB:$HotfixID /quiet /norestart"
+    $null = ([WMICLASS]"\\$computername\ROOT\CIMV2:win32_process").Create($UninstallString) 
+
+    while (@(Get-Process -Name wusa -ComputerName $computername -ErrorAction SilentlyContinue).Count -ne 0) 
+    {
+      Start-Sleep -Seconds 3
+      Write-Verbose -Message 'Waiting for update removal to finish ...'
     }
-write-host "Completed the uninstallation of $hotfixID"
-}
-else {            
+    Write-Verbose -Message ('Completed the uninstallation of {0}' -f $HotfixID)
+  }
+  else 
+  {
+   
+    Write-Verbose "List of installed Hotfixes:"
+    Write-Verbose $hotfixes
 
-write-host "Given hotfix($hotfixID) not found"
-return
-}            
-
+    Write-Output -InputObject ('Given hotfix({0}) not found' -f $HotfixID)
+    return
+  }
 }
