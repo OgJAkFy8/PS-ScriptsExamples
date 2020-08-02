19a5fdc3b1323411aafb6c347a7156f544f85973
Erik Arnesen <earnesen@ymail.com>
2020-08-02 10:14:39 -0400

    Update try-catchExample.ps1

--- _Duplicates/try-catchExample.ps1
+++ _Duplicates/try-catchExample.ps1
@@ -10,11 +10,7 @@ Write-Host "Press any key to exit ..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
 }
-# NOTE: When you use a SPECIFIC catch block, exceptions thrown by -ErrorAction Stop MAY LACK
-# some InvocationInfo details such as ScriptLineNumber.
-# REMEDY: If that affects you, remove the SPECIFIC exception type [System.NotImplementedException] in the code below
-# and use ONE generic catch block instead. Such a catch block then handles ALL error types, so you would need to
-# add the logic to handle different error types differently by yourself.
+
 catch [System.NotImplementedException]
 {
   # get error record
