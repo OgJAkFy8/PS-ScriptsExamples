df1fef8827c04e512a2e564290ec0b8b378d27d1
Erik Arnesen <earnesen@ymail.com>
2020-08-03 07:21:53 -0400

    Create Test-AuthentationServer.ps1

---
+++ Test-AuthentationServer.ps1
@@ -0,0 +1,15 @@
+﻿function Test-AuthentationServer
+{
+  try
+  {
+    # Check if computer is connected to domain network
+    [void]::([System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain())
+    Write-Output -InputObject ('Authentication Server: {0}' -f $env:LOGONSERVER)
+  }
+  catch
+  {
+    Write-Output -InputObject ('Authentication Server: Not Available') 
+    Write-Output -InputObject ('Local Workstation: {0}' -f $env:COMPUTERNAME)
+  }
+}
+    
