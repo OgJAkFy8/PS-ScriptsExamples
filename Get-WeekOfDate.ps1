d67f5dbddcee06fa389fc4e974cbc68f66eb4bd6
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-08-03 08:40:57 -0400

    Create Get-WeekOfDate.ps1

---
+++ Get-WeekOfDate.ps1
@@ -0,0 +1,37 @@
+﻿function Get-WeekOfDate
+{
+  <#
+    .SYNOPSIS
+    Get the "week of" date starting on Monday.  
+
+    Great for the "Week Of August-03 Report"
+
+  #>
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory=$false, Position=0)]
+    [System.String]
+    $dateFormat = 'MMMM-dd'
+  )
+  
+  $CurrentDay = Get-Date -UFormat %A
+  Switch ($CurrentDay){
+    Monday 
+    {    $CurrentWeek = (Get-Date).AddDays(0).ToString($dateFormat)  }
+    Tuesday 
+    {    $CurrentWeek = (Get-Date).AddDays(-1).ToString($dateFormat)  }
+    Wednesday 
+    {    $CurrentWeek  = (Get-Date).AddDays(-2).ToString($dateFormat)  }
+    Thursday 
+    {    $CurrentWeek = (Get-Date).AddDays(-3).ToString($dateFormat)  }
+    Friday 
+    {    $CurrentWeek = (Get-Date).AddDays(-4).ToString($dateFormat)  }
+    Saturday 
+    {    $CurrentWeek = (Get-Date).AddDays(-5).ToString($dateFormat)  }
+    Sunday 
+    {    $CurrentWeek = (Get-Date).AddDays(-6).ToString($dateFormat)  }
+  }
+  $CurrentWeek
+}
+
