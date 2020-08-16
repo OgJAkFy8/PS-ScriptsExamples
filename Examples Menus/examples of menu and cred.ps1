647b2fc5d0735c5a127afa6addd7976e1f92260c
Erik Arnesen <earnesen@ymail.com>
2020-08-16 06:58:52 -0400

    Update and move

---
+++ Examples Menus/examples of menu and cred.ps1
@@ -0,0 +1,27 @@
+﻿<#$Credential = Get-Credential
+$Credential | Export-CliXml -Path .\Jaap.Cred
+$Credential = Import-CliXml -Path .\Jaap.Cred
+#>
+
+$MenuObject = 'System.Management.Automation.Host.ChoiceDescription'
+$red1 = New-Object -TypeName $MenuObject -ArgumentList '&Red1', 'Favorite color: Red1'
+$blue1 = New-Object -TypeName $MenuObject -ArgumentList '&Blue1', 'Favorite color: Blue1'
+$yellow1 = New-Object -TypeName $MenuObject -ArgumentList '&Yellow1', 'Favorite color: Yellow1'
+$red2 = New-Object -TypeName $MenuObject -ArgumentList '&Red2', 'Favorite color: Red2'
+$blue2 = New-Object -TypeName $MenuObject -ArgumentList '&Blue2', 'Favorite color: Blue2'
+$yellow2 = New-Object -TypeName $MenuObject -ArgumentList '&Yellow2', 'Favorite color: Yellow2'
+$red3 = New-Object -TypeName $MenuObject -ArgumentList '&Red3', 'Favorite color: Red3'
+$blue3 = New-Object -TypeName $MenuObject -ArgumentList '&Blue3', 'Favorite color: Blue3'
+$yellow3 = New-Object -TypeName $MenuObject -ArgumentList '&Yellow3', 'Favorite color: Yellow3'
+$red4 = New-Object -TypeName $MenuObject -ArgumentList '&Red4', 'Favorite color: Red4'
+$blue4 = New-Object -TypeName $MenuObject -ArgumentList '&Blue4', 'Favorite color: Blue4'
+$yellow4 = New-Object -TypeName $MenuObject -ArgumentList '&Yellow4', 'Favorite color: Yellow4'
+ 
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($red1, $blue1, $yellow1,$red2, $blue2, $yellow2,$red3, $blue3, $yellow3,$red4, $blue4, $yellow4)
+
+
+$title = 'Favorite color'
+$message = 'What is your favorite color?'
+$result = $host.ui.PromptForChoice($title, $message, $options, 0)
+
+Write-Output $result
---
+++ Services-testing output.ps1
@@ -0,0 +1,67 @@
+﻿#requires -Version 4.0
+
+function Show-Output
+{
+  param
+  (
+    [Parameter(Mandatory, ValueFromPipeline, HelpMessage = 'Data to filter')]
+    [Object]$InputObject,
+    [Parameter(Mandatory,HelpMessage = 'Service Status')]
+    [ValidateSet('Running','Stopped')]
+    [Object]$Status
+  )
+  
+  $AsciiCheckBox = '[ ]'
+  $DoubleLineBoarder = "`n=============================`n"
+
+  If($Status -eq 'Running')
+  {
+    $HighLightColor = 'Green'
+  }
+  else
+  {
+    $HighLightColor = 'Red'
+  }
+
+  If ($InputObject.count -ne 0)
+  {
+    Write-Host $AsciiCheckBox -foreground White -BackgroundColor $HighLightColor -NoNewline
+    Write-Output -InputObject " Services $Status"
+    $InputObject
+    Write-Host $DoubleLineBoarder -foregroundcolor $HighLightColor
+  }
+}
+
+function Get-ServiceList
+{
+  param
+  (
+    [Parameter(Mandatory, ValueFromPipeline, HelpMessage = 'Data to filter')]
+    [Object]$InputObject,
+    [Parameter(Mandatory,HelpMessage = 'Servive Status')]
+    [ValidateSet('Running','Stopped')]
+    [Object]$Status,
+    [Parameter(Mandatory,HelpMessage = 'Service Starup Type')]
+    [ValidateSet('Automatic','Manual')]
+    [Object]$StartType
+  )
+  process
+  {
+    if (($InputObject.Status -eq $Status) -and ($InputObject.StartType -eq $StartType))
+    {
+      $InputObject
+    }
+  }
+}
+
+$ServiceStopped = Get-Service | Get-ServiceList -Status Stopped -StartType Automatic
+
+$ServiceRunning = Get-Service | Get-ServiceList -Status Running -StartType Automatic
+
+
+$ServiceRunning | Show-Output -Status Running
+$ServiceStopped | Show-Output -Status Stopped
+
+#
+
+
