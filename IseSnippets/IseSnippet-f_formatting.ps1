c4cc7465051d3b32766b1875d04d0e91bf41bce5
Erik Arnesen <earnesen@ymail.com>
2020-09-20 21:20:29 -0400

    Clean up

---
+++ IseSnippets/IseSnippet-f_formatting.ps1
@@ -0,0 +1,16 @@
+﻿$m = @'
+<#
+A column for the 1st item of 4 characters, left-aligned and adds a space
+A column for the 2nd item of 20 characters left-aligned and adds a space
+A column for the 3rd item of 20 characters right-aligned.
+#>
+
+$Item1 = @{Item = 1}
+$Item2 = 'Left Aligned'
+$Item3 = 'Right Aligned'
+
+('{0,-4}:{1,-20}:{2,20}' -f $Item1.Item, $Item2, $Item3)
+'@
+New-IseSnippet -Text $m -Title 'Format -f Operator' -Description 'Output format with -f operator' -Author 'Knarr Studio'
+
+
