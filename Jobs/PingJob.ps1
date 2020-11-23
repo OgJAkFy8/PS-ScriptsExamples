a89e318dd11cb32b9b9123a2330c9692484fe750
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-11-22 20:55:50 -0500

    Working with jobs

---
+++ Jobs/PingJob.ps1
@@ -0,0 +1,26 @@
+﻿$sites = 'yahoo.com', 'google.com', 'bing.com'
+$Jobs = @()
+$job = {Start-Job -ScriptBlock {
+  param ($site) Test-Connection -ComputerName "$site" -Count 1
+}}
+        
+for ($i = 0; $i -lt $sites.Count; $i++)
+{
+  $site = [string]$sites[$i]
+  & $job -site $site
+}
+        
+
+
+
+$job = Test-Connection ('yahoo.com', 'google.com', 'bing.com') -AsJob -Count 1
+for ($i = 0; $i -lt $sites.Count; $i++)
+{
+  Start-Sleep -Seconds 2
+  Write-Host $sites[$i]
+}
+$Results = Receive-Job $job -Wait
+$Results[1].Address
+
+
+
