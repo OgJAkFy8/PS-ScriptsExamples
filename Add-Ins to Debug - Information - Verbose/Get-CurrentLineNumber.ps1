2ade6b2b958f05fce9afebe15554f48910f87fa2
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-11-28 07:30:02 -0500

    Current Line Number edit

---
+++ Add-Ins to Debug - Information - Verbose/Get-CurrentLineNumber.ps1
@@ -0,0 +1,31 @@
+﻿#requires -Version 3.0
+  <#
+      .SYNOPSIS
+      Add-In to aid troubleshooting. A quick way to write where in the code you are
+
+      .EXAMPLE
+      Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 7 ),'') 
+  #>
+function Get-CurrentLineNumber
+{
+  param
+  (
+    [Parameter(Mandatory,Position = 0)]
+    [int]$MsgNum
+  )
+  $VerboseMsg = @{
+    1 = 'Set Variable'
+    2 = 'Set Switch Variable'
+    3 = 'Set Path/FileName'
+    4 = 'Start Function'
+    5 = 'Start Loop'
+    6 = 'End Loop'
+    7 = 'Write Data'
+    99 = 'Current line number'
+  }
+  if($MsgNum -gt $VerboseMsg.Count)
+  {
+    $MsgNum = 99
+  }#$VerboseMsg.Count}
+  'Line {0}:  {1}' -f $MyInvocation.ScriptLineNumber, $($VerboseMsg.$MsgNum)
+} 
