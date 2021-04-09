b16eaeb6316c0a2e361c3e33a7c6e2196608b913
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-04-08 21:16:42 -0400

    Get variables from file

---
+++ Example Config file/Get-ConfigData.ps1
@@ -0,0 +1,31 @@
+﻿# Example 1: Hash Table with RegEx
+Get-Content .\Config.txt | 
+foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'=');
+ if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } 
+ }
+Write-Host 'Example 1: Hash Table with RegEx -' $h.Color
+
+
+# Example 2: Hash Table
+$hashset = @{}
+$dlm = '='
+$settings = Get-Content .\Settings.ini
+foreach($line in $settings)
+    {
+    $key = $line -split $dlm, 2
+    #$hashset.Add($key[0],$key[1..($key.Length -1)] -join "=")
+    $hashset.Add($key[0],$key[1])
+    }
+Write-Host 'Example 2: Hash Table -' $hashset.Color
+
+
+# Example 3: JSON
+$json = Get-Content .\config.json | ConvertFrom-Json
+Write-Host 'Example 3: JSON -' $json.Color
+
+
+# Example 4: JSON with headings
+$json = Get-Content .\config2.json | ConvertFrom-Json
+Write-Host 'Example 4: JSON with headings -' $json.Other.Color
+
+
