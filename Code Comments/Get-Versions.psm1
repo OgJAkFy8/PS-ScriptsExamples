bc16edcbc8c0780db6a0b90b3866e869766de274
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-07-19 23:59:24 -0400

    Create Get-Versions.psm1

---
+++ Code Comments/Get-Versions.psm1
@@ -0,0 +1,55 @@
+﻿#!/usr/bin/env powershell
+
+function Get-Versions
+{
+  <#
+      .SYNOPSIS
+      A way to get the OS version you are running
+  #>
+  [CmdletBinding()]
+  param
+  ([Parameter(Mandatory = $false, Position = 0)]$input
+  
+  )
+    
+    
+  [String]$MagMinVer = '{0}.{1}'
+    
+  [Float]$PsVersion = ($MagMinVer -f [int]($psversiontable.PSVersion).Major, [int]($psversiontable.PSVersion).Minor)
+  
+  
+  #requires -Version 1.0
+  
+  
+  if($PsVersion  -ge 6)
+  {
+    if ($IsLinux) 
+    {
+      $OperatingSys = 'Linux'
+    }
+    elseif ($IsMacOS) 
+    {
+      $OperatingSys = 'macOS'
+      $OsMac
+    }
+    elseif ($IsWindows) 
+    {
+      $OperatingSys = 'Windows'
+    }
+  }
+  Else
+  {
+    Write-Output -InputObject ($MagMinVer -F ($(($psversiontable.PSVersion).Major), $(($psversiontable.PSVersion).Minor)))
+    if($env:os)
+    {
+      $OperatingSys = 'Windows'
+    }
+  }
+  $x = @{
+    PSversion = $PsVersion
+    OSVersion = $OperatingSys
+  }
+  
+  return $x
+}
+
