f87d13f35fd538667f5c995207fb1508031f1512
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-09-05 09:07:30 -0500

    Create Write-ToConsole.ps1

---
+++ Write-ToConsole/Write-ToConsole.ps1
@@ -0,0 +1,26 @@
+﻿Function Write-ToConsole {
+  [CmdletBinding(DefaultParameterSetName = 'Set 1')]
+  param
+  (
+    [Parameter(Mandatory)][string]$Details,
+    [Parameter(ParameterSetName = 'Set 1')]
+    [Parameter(ParameterSetName = 'Set 2')]
+    [switch]$Green,
+
+    [Parameter(ParameterSetName = 'Set 1')]
+    [Parameter(ParameterSetName = 'Set 3')]
+    [switch]$Red,
+
+    [Parameter(ParameterSetName = 'Set 1')]
+    [Parameter(ParameterSetName = 'Set 4')]
+    [switch]$Yellow
+  )
+  $Color = 'Gray'
+  if($Green){$Color = 'Green'}
+  if($Red{$Color = 'Red'}
+  if($Yellow{$Color = 'Yellow'}
+
+  $LogDate = Get-Date -Format T
+  Write-Host -Object ('{0} {1}' -f ($LogDate), $Details) -ForegroundColor $Color
+}
+
