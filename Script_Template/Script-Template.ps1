7647801de34261f794b35d1a8bbaa0cf3eafb725
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-09-05 09:10:23 -0500

    Create Script-Template.ps1

---
+++ Script_Template/Script-Template.ps1
@@ -0,0 +1,31 @@
+﻿#requires -Version 2.0 
+<#
+    .Synopsis
+    This is a simple script template
+    .DESCRIPTION
+    Long description
+    .EXAMPLE
+    Example of how to use this cmdlet
+    .EXAMPLE
+    Another example of how to use this cmdlet
+#>
+
+[CmdletBinding()]
+Param
+(
+  # Output filename
+  [Parameter(Mandatory = $false,ValueFromPipelineByPropertyName = $true)]
+  [String]$OutputFile
+)
+
+if(-not (Get-Module -Name ITPS.OMCS.CodingFunctions))
+{
+  Import-Module -Name '.\ITPS.OMCS.CodingFunctions.psm1'
+}
+if($OutputFile)
+{
+  $OutputFile = New-File -Filename $OutputFile -Increment -Tag $(Get-Date -UFormat %j)
+}
+
+
+
