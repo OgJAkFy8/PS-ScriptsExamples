0a13816f9223cf8de06346e5ecb32547688549bd
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-09-05 09:11:13 -0500

    Create Export-ModulesExample.psm1

---
+++ Modules/Export-ModulesExample.psm1
@@ -0,0 +1,15 @@
+﻿function Set-Name{
+    Param(
+        [Parameter( Mandatory = $false)]
+        $name = "JoBommo"    
+    )
+    Return $name
+}
+
+function Write-Name {
+
+Set-Name (Read-Host "Name")
+Write-Host $name
+}
+
+Export-ModuleMember Write-Name
