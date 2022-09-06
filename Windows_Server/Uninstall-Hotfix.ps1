be3eb762550afbb6b968c62fa9d0a1a2072d8569
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-09-05 20:52:35 -0500

    Update Uninstall-Hotfix.ps1

--- Windows_Server/Uninstall-Hotfix.ps1
+++ Windows_Server/Uninstall-Hotfix.ps1
@@ -1,29 +1,28 @@
-function Uninstall-Hotfix {
-[cmdletbinding()]
-param(
-$computername = $env:computername,
-[string] $HotfixID
-)            
-
-$hotfixes = Get-WmiObject -ComputerName $computername -Class Win32_QuickFixEngineering | select hotfixid            
-
-if($hotfixes -match $hotfixID) {
-    $hotfixID = $HotfixID.Replace("KB","")
-    Write-host "Found the hotfix KB" + $HotfixID
-    Write-Host "Uninstalling the hotfix"
-    $UninstallString = "cmd.exe /c wusa.exe /uninstall /KB:$hotfixID /quiet /norestart"
-    ([WMICLASS]"\\$computername\ROOT\CIMV2:win32_process").Create($UninstallString) | out-null            
-
-    while (@(Get-Process wusa -computername $computername -ErrorAction SilentlyContinue).Count -ne 0) {
-        Start-Sleep 3
-        Write-Host "Waiting for update removal to finish ..."
+function Uninstall-Hotfix 
+{
+  [cmdletbinding()]
+  param(
+    [String]$computername = $env:computername,
+    [String] $HotfixID
+  )            
+  $hotfixes = (Get-WmiObject -ComputerName $computername -Class Win32_QuickFixEngineering).Hotfixid  #| Select-Object -Property hotfixid            
+  if($hotfixes -match $HotfixID) 
+  {
+    Write-Verbose -Message 'Found the hotfix {0}' -f $HotfixID
+    Write-Verbose -Message 'Uninstalling the hotfix'
+    $UninstallString = ('cmd.exe /c wusa.exe /uninstall /KB:{0} /quiet /norestart' -f $($HotfixID.Replace('KB','')))
+    $null = ([WMICLASS]"\\$computername\ROOT\CIMV2:win32_process").Create($UninstallString)            
+    while (@(Get-Process -Name wusa -ComputerName $computername -ErrorAction SilentlyContinue).Count -ne 0) 
+    {
+      Start-Sleep -Seconds 3
+      Write-Verbose -Message 'Waiting for update removal to finish ...'
     }
-write-host "Completed the uninstallation of $hotfixID"
+    Write-Verbose -Message ('Completed the uninstallation of {0}' -f $HotfixID)
+  }
+  else 
+  {
+    Write-Verbose -Message ('Given hotfix({0}) not found' -f $HotfixID)
+    return
+  }
 }
-else {            
 
-write-host "Given hotfix($hotfixID) not found"
-return
-}            
-
-}
