de8096d0b29065d28118d41133c21d2e90205234
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-09-05 20:48:59 -0500

    Create Convert-ToFromAscii.ps1

---
+++ ASCII Conversion/Convert-ToFromAscii.ps1
@@ -0,0 +1,11 @@
+﻿Function ConvertTo-Ascii ($String) {
+($String.ToCharArray() | %{([byte][char]$_)}) -join '|'
+}
+
+# Ascii Example: '101|114|105|107|64|69|77|65|73|76|46|99|111|109'
+Function ConvertFrom-Ascii ($AsciiString) {
+($AsciiString.Split('|') | %{[Char][Byte]$_}) -join ''
+}
+
+# Oneliner
+# ('101|114|105|107|64|69|77|65|73|76|46|99|111|109'.Split('|') | %{[Char][Byte]$_ }) -join ''
