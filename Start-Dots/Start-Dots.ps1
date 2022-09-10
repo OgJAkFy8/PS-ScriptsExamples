5cb59fc0245f52b67e8206ed65bbad961367d600
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-09-10 09:21:48 -0500

    Create Start-Dots.ps1

---
+++ Start-Dots/Start-Dots.ps1
@@ -0,0 +1,22 @@
+﻿function Start-Dots
+{
+  <#
+      .SYNOPSIS
+      Shows start dots
+  #>
+  if (-not (Test-Path -Path variable:global:psISE))
+  {
+    Clear-Host
+    $i = 0 
+    do 
+    { 
+      Start-Sleep -Milliseconds 100
+      Write-Host -Object '.  ' -NoNewline -ForegroundColor ('Green', 'Red', 'Cyan', 'Yellow' | Get-Random)
+      $i++
+    }
+    until (($i -gt 20) -or [System.Console]::KeyAvailable)  
+    Write-Host -Object "`n"
+  }
+}
+
+Start-Dots
