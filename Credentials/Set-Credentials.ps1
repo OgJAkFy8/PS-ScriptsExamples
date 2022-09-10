1eb95a5abe227111773041e2088382ff3fba7b7c
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-09-10 09:22:44 -0500

    Create Set-Credentials.ps1

---
+++ Credentials/Set-Credentials.ps1
@@ -0,0 +1,14 @@
+﻿param(
+[Parmeter(Mandatory=$false)]
+[ValidateSet('Domain','Local')]
+$AccountType = 'Local'
+)
+if($AccountType -eq 'Local'){
+    $AccountType = $env:COMPUTERNAME
+    }
+if($AccountType -eq 'Domain'){
+    $AccountType = $env:USERDOMAIN
+    }
+$CredFile = '{0}\.PsCredentials\{1}-{2}' -f $env:USERPROFILE,$AccountType,$env:USERNAME
+Get-Credential | Export-CliXml -Path $CredFile
+$Credential = Import-CliXml -Path $CredFile
